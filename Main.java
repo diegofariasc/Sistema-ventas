47ce6117837028becd76ba5e7db329df0bbad306
Diego Farias Castro <dfc_2014@hotmail.com>
2020-02-06 01:42:09 -0600

    Creada InterfazProductos

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
 public class Main{
     public static void main (String[] args){
 
+        InterfazProductos productos = new InterfazProductos();
+        productos.iniciarInterfaz();
+
     } //End main
 } //End class
