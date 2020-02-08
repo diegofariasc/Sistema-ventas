097ced8e0a77a52195fd3125a5b8a9b11dd47546
Diego Farias Castro <dfc_2014@hotmail.com>
2020-02-08 13:54:54 -0600

    Creada BaseDatosProductos

---
+++ BaseDatosProductos.java
@@ -0,0 +1,45 @@
+public class BaseDatosProductos implements Model{
+
+    @Override
+    public void cargaDatosDelRepositorioALaEstructura() {
+
+    } //End cargaDatosDelRepositorioALaEstructura
+
+    @Override
+    public void salvaDatosDeLaEstructuraAlRepositorio() {
+
+    } //End salvaDatosDeLaEstructuraAlRepositorio
+
+    @Override
+    public void agregaDatosALaEstructura(int indice, Object dato) {
+
+    } //End agregaDatosALaEstructura
+
+    @Override
+    public void modificaDatosEnLaEstructura(int indice, Object dato) {
+
+    } //End modificaDatosEnLaEstructura
+
+    @Override
+    public void eliminaDatosDeLaEstructura(int indice) {
+
+    } //End eliminaDatosDeLaEstructura
+
+    @Override
+    public void ordenaLaEstructura() {
+
+    } //End ordenaLaEstructura
+
+    @Override
+    public double procesa(int indice) {
+        
+        return 0;
+    } //End procesa
+
+    @Override
+    public boolean hayDatos() {
+
+        return false;
+    } //End hayDatos
+
+} //End class
